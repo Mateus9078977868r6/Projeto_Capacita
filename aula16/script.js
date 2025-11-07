2e758e5172601f5340fc20fddc0eb6bb391ac17b
Mateus Batista Macieira <0117316@ifmg.edu.br>
2025-11-07 15:26:37 -0300

    cria pasta aula16 e inclui html e script.js

---
+++ aula16/script.js
@@ -0,0 +1,13 @@
+class Pessoa{
+    constructor(nome, idade){
+        this.nome = nome;
+        this.idade = idade;
+    }
+    apresentar(){
+        console.log(this.nome + " tem " + this.idade + " anos");
+    }
+}
+let p1 = new Pessoa("Ana",30);
+let p2 = new Pessoa("Bia",40);
+p1.apresentar();
+p2.apresentar();
