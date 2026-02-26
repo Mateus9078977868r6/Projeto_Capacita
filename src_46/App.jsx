ab61ff5601017d60af104429ca13657bc7a99cf1
Mateus_BM <164224906+Mateus9078977868r6@users.noreply.github.com>
2026-02-26 16:33:27 -0300

    Add files via upload
    Conceitos de REST, utilizando saídas json

---
+++ src_46/App.jsx
@@ -0,0 +1,24 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+    .then(res => res.json())
+    .then(data => setPosts(data));
+  },[]);
+
+  return (
+    <ul>
+      {posts.map(post=>(
+        <li key={post.id}>{post.title}</li>
+      ))}
+    </ul>
+  )
+}
+
+export default App
